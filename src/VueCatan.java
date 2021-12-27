22696acd6fc5bf82c14a0e943b0cab7a3f83bab8
Shuaib <shuaib.2106@gmail.com>
2021-12-27 16:58:27 +0100

    m

--- src/VueCatan.java
+++ src/VueCatan.java
@@ -6,16 +6,24 @@ import java.io.File;
 import java.io.IOException;
 
 public class VueCatan extends JFrame {
+    protected ModeleCatan model;
+    private ImagePane imagePane;
+
     public VueCatan() {
         setTitle("Catan");
-        setSize(800, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        model = new ModeleCatan("./Image/Ocean.jpeg");
+        imagePane = new ImagePane();
+        setContentPane(imagePane);
+
+        this.add(new JPanel(), BorderLayout.CENTER);
+        this.setLocationRelativeTo(null);
     }
 
-    public class ImageEditModel {
+    private class ModeleCatan {
         private BufferedImage image;
 
-        public ImageEditModel(String chemin) {
+        public ModeleCatan(String chemin) {
             try {
                 image = ImageIO.read(new File(chemin));
             } catch (IOException e) {
@@ -26,5 +34,29 @@ public class VueCatan extends JFrame {
         public BufferedImage getImage() {
             return image;
         }
+
+    }
+
+    private class ImagePane extends JPanel {
+
+        public ImagePane() {
+            setPreferredSize(
+                    new Dimension(model.getImage().getWidth(), model.getImage().getHeight()));
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(model.getImage(), 0, 0, this);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            VueCatan view = new VueCatan();
+            view.pack();
+            view.setVisible(true);
+        });
     }
 }
